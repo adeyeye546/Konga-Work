31261f4359d7c182ab840ea2493b90c16f3a01d3
Gbenga Sam <adeyeye546@gmail.com>
2019-02-20 10:05:52 +0100

    using vue to display new products

---
+++ js/myproduct.js
@@ -0,0 +1,21 @@
+Vue.component('myproduct', {
+    template: `
+    <div class="container new-arrival"> 
+                <div class="row">
+                    <div class="col">
+                        <h5 class="title">NEW ARRIVAL</h5>
+                    </div>
+                </div>
+                <div class="row">
+                
+                    <div class="item col-3">
+                        <img src="img/a.JPG" alt="">
+                        <p class="product-name">Lazer Printer</p>
+                        <p class="price">#12,000</p>
+
+                    </div>
+                   
+                    </div>
+            </div>
+    `
+})
